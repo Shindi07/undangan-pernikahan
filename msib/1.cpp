793b1b5b8f53a44fbcacdad2d516e60b2afb9519
Shindi07 <shindicahyandari8@gmail.com>
2024-02-25 14:29:18 +0700

    first commit

---
+++ msib/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+   int nilai=15;
+      for (int i=1; i<=nilai; i++){
+          if (i% 2! =0){
+
+            std::cout<<i<<"";
+       
+          
+      }
+ 
+  cout << endl;
+  return 0;
+}
+}
